1db6c4442e98aeead741c18247fc25841da13dd4
Gavin Gray <gavingray1729@gmail.com>
2015-05-15 15:44:53 +0100

    first version of test for run module

---
+++ tests/test_run.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+############################
+# Tests for the run module #
+############################
+
+import unittest
+import mock
+import pytest
+
+from .context import holonets.run
+
+class TestHolonetsRun(unittest.TestCase):
+    """
+    To test that the run module can take a training function, run it over 
+    epochs and accumulate results in a HoloMap object.
+    """
+    @classmethod
+    def setUpClass(self):
+        # create mock reqs
+        self.channel_name = 'Dummy Channel'
+        self.train_function = mock.MagickMock(return_value={
+            self.channel_name: 1 
+            })
+
+        self.train_loop = holonets.run.EpochLoop(self.train_function)
+
+    def test_iterate(self):
+        """
+        Attempt to run training loop for a given number of epochs.
+        """
+        N_epochs = 10
+        test_holomap = self.train_loop.run(10)
+        # check holomap contains the right data
+        assert test_holomap[self.channel_name].data == np.ones(10,2)
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
