9d6eb8c613bc086a4be738693fb9c1f69c01bc26
Gavin Gray <gavingray1729@gmail.com>
2015-05-15 12:36:10 +0100

    building repo structure

---
+++ tests/context.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# following recommended practice from
+# http://www.kennethreitz.org/essays/repository-structure-and-python
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import holonets
