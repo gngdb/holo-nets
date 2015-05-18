1e1a81a8d6a71c1a868ac57c9483ce1f63448afa
Gavin Gray <gavingray1729@gmail.com>
2015-05-18 12:51:07 +0100

    tests now pass, and check for multiple output functions added

--- holonets/train.py
+++ holonets/train.py
@@ -7,10 +7,11 @@ class Train:
     Iterator class that runs theano functions over data while gathering
     the resulting monitoring values for plotting.
     """
-    def __init__(self, *channels, n_batches={'train':1, 
-                                             'validation':1, 
-                                             'test':1}):
+    def __init__(self, channels, n_batches={'Train':1, 
+                                            'Validation':1, 
+                                            'Test':1}):
         """
+        Channels are passed as list or tuple "channels" of dictionaries.
         Expecting each channel as a dictionary with the following entries:
         - "names": <Names of channels as tuple of strings>
         - "dataset": <Which dataset to average this value over (one of train, 
@@ -20,7 +21,7 @@ class Train:
         index to slice into a shared variable dataset>
         - "dimensions": <value dimension as string or Holoviews Dimension>
         Also, need to know how many batches are in the dataset to iterate over:
-        - n_batches - number of batches in the dataset
+        - n_batches - number of batches in the dataset, as above, as dictionary.
         """
         self.n_batches = n_batches
         # make a dictionary of channel:[dimension]
@@ -28,7 +29,7 @@ class Train:
         for channel in channels:
             dimension = channel.get('dimensions',False)
             if dimension:
-                self.dimensions[channel['names']] = [dimension]
+                self.dimensions[channel['names']] = enforce_iterable(dimension)
         
         # store channels
         self.channels = channels
@@ -39,13 +40,13 @@ class Train:
     def next(self):
         self.collected_channels = {}
         # iterate over train, validation and test channels:
-        for dataset_name in ['train', 'validation', 'test']:
+        for dataset_name in ['Train', 'Validation', 'Test']:
             # gather right channels for this dataset
             channels = [channel for channel in self.channels 
                     if channel['dataset'] == dataset_name]
             # check we have some channels to iterate over
             if channels != []:
-                for i in range(n_batches[dataset_name]):
+                for i in range(self.n_batches[dataset_name]):
                     # on each batch, execute functions for training channels and
                     # gather results
                     for channel in channels:
@@ -68,13 +69,13 @@ class Train:
             # inputs later)
             returned_vals = enforce_iterable(channel['eval']())
             for name, rval in zip(channel['names'], returned_vals):
+                self.collected_channels[name] = rval
                 
         return self.collected_channels
 
 def enforce_iterable(foo):
     """
-    Function to make sure anything passed to it is iterable, even if it's 
-    not.
+    Function to make sure anything passed to it is returned iterable.
     """
     if not hasattr(foo, '__iter__'):
         foo = [foo]
--- tests/test_train.py
+++ tests/test_train.py
@@ -21,37 +21,37 @@ class TestHolonetsTrain(unittest.TestCase):
     def setUpClass(self):
         # set up rng
         rng = np.random.RandomState(42)
-        self.rval = rng.randn(3)
+        self.rval = rng.randn(4)
         # make mock Theano functions
-        fprop_train = mock.MagicMock(return_value=self.rval[0])
-        fprop_test = mock.MagicMock(return_value=self.rval[1])
-        independent = mock.MagicMock(return_value=self.rval[2])
+        fprop_train = mock.MagicMock(return_value=(self.rval[0],self.rval[1]))
+        fprop_test = mock.MagicMock(return_value=self.rval[2])
+        independent = mock.MagicMock(return_value=self.rval[3])
         # train function expects to take these as dictionary with
         # entries to indicate whether to run on train, test or eval 
         # independently after running an epoch
         # Also, should contain the name of the channel
         self.train_channel = {
-                'names':['Train Channel'],
+                'names':('Train Channel','Train Channel 2'),
                 'dataset':'Train',
                 'eval':fprop_train,
-                'dimensions':['Loss']
+                'dimensions':['Loss', 'Misc']
         }
         self.test_channel = {
-                'names':['Test Channel'],
+                'names':('Test Channel',),
                 'dataset':'Test',
                 'eval':fprop_test
         }
         self.independent = {
-                'names':['Independent Channel'],
+                'names':('Independent Channel',),
                 'dataset':'None',
                 'eval':independent
         }
 
         # assuming *args interface
         self.train = holonets.train.Train(
-                self.train_channel, 
+               [self.train_channel, 
                 self.test_channel, 
-                self.independent
+                self.independent]
         )
 
     def test_traincall(self):
@@ -63,17 +63,18 @@ class TestHolonetsTrain(unittest.TestCase):
         output_dict = next(iter(self.train))
         for i,channel in enumerate([
                        'Train Channel',
+                       'Train Channel 2',
                        'Test Channel',
                        'Independent Channel']):
-            assert np.allclose(output_dict[channel], rval[i])
+            assert np.allclose(output_dict[channel], self.rval[i])
 
     def test_dimensions(self):
         """
         Check that it's easily to pull the dimensions back out of this object.
         """
         # want to be able to pull dimensions out using the channels name
-        dimensions = self.train.dimensions['Train Channel']
-        assert dimensions == ['Loss']
+        dimensions = self.train.dimensions[('Train Channel','Train Channel 2')]
+        assert dimensions == ['Loss', 'Misc']
 
 def main():
     unittest.main()
