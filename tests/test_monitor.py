1ada8601e56991ddf6313176096fc88f7a1afe8f
Gavin Gray <gavingray1729@gmail.com>
2015-05-18 17:03:24 +0100

    empty test module

---
+++ tests/test_monitor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+################################
+# Tests for the monitor module #
+################################
+
+import unittest
+import mock
+import pytest
+import numpy as np
+
+from .context import holonets
+
+class TestHolonetsMonitor(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        pass
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
